5d9efaa08808b1151746afb018f4a50355444301
shaikhfurquan <shaikhfurquan313@gmail.com>
2025-08-20 15:23:18 +0530

    get single order admin api

--- backend/controllers/order.controller.js
+++ backend/controllers/order.controller.js
@@ -15,24 +15,39 @@ export const createNewOrder = handleAsyncError(async (req, res, next) => {
         taxPrice = 0,
         shippingPrice,
         totalPrice,
-               
+
     } = req.body;
 
-    const order = await OrderModel.create( {
+    const order = await OrderModel.create({
         shippingInfo,
         orderItems,
         paymentInfo,
         itemPrice,
-        taxPrice ,
+        taxPrice,
         shippingPrice,
         totalPrice,
-        paidAt : Date.now(),
-        user : req.user._id
-               
+        paidAt: Date.now(),
+        user: req.user._id
+
     })
 
     res.status(200).json({
-        success : true,
+        success: true,
+        order
+    })
+
+})
+
+
+// Admin - getting th single order
+export const getSingleOrder = handleAsyncError(async (req, res, next) => {
+    const order = await OrderModel.findById(req.params.orderId).populate("user", "name email")
+    if (!order) {
+        return next(new HandleError("Order not found", 404))
+    }
+
+    res.status(200).json({
+        success: true,
         order
     })
 
--- backend/routes/order.route.js
+++ backend/routes/order.route.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import { isUserAuthenticated, roleBasedAccess } from '../middlewares/isAuth.middleware.js';
-import { createNewOrder } from '../controllers/order.controller.js';
+import { getSingleOrder, createNewOrder } from '../controllers/order.controller.js';
 
 
 
 const orderRouter = express.Router();
 
-orderRouter.post('/new/order' , isUserAuthenticated , createNewOrder)
+orderRouter.post('/new/order', isUserAuthenticated, createNewOrder)
+orderRouter.get('/admin/get/:orderId', isUserAuthenticated, roleBasedAccess('admin'), getSingleOrder)
 
 export default orderRouter;
