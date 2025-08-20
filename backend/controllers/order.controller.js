b0b340a19f270efec31ef6bf11b759daf812f72a
shaikhfurquan <shaikhfurquan313@gmail.com>
2025-08-20 16:03:44 +0530

    my orders annd admin get orders

--- backend/controllers/order.controller.js
+++ backend/controllers/order.controller.js
@@ -52,3 +52,41 @@ export const getSingleOrder = handleAsyncError(async (req, res, next) => {
     })
 
 })
+
+
+// my all order (login user) 
+export const getMyAllOrders = handleAsyncError(async (req, res, next) => {
+    const myOrders = await OrderModel.find({ user: req.user._id })
+    if (!myOrders) {
+        return next(new HandleError("Order not found", 404))
+    }
+
+    res.status(200).json({
+        success: true,
+        totalOrders: myOrders.length,
+        myOrders
+    })
+
+})
+
+
+// Admin -  get all order 
+export const getAllOrders = handleAsyncError(async (req, res, next) => {
+    const allOrders = await OrderModel.find()
+
+    let totalAmount = 0
+    allOrders.forEach(order => {
+        totalAmount += order.totalPrice
+    })
+    if (!allOrders) {
+        return next(new HandleError("Orders not found", 404))
+    }
+
+    res.status(200).json({
+        success: true,
+        allOrdersCount: allOrders.length, 
+        totalAmount,
+        allOrders
+    })
+
+})
--- backend/routes/order.route.js
+++ backend/routes/order.route.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { isUserAuthenticated, roleBasedAccess } from '../middlewares/isAuth.middleware.js';
-import { getSingleOrder, createNewOrder } from '../controllers/order.controller.js';
+import { getSingleOrder, createNewOrder, getMyAllOrders, getAllOrders } from '../controllers/order.controller.js';
 
 
 
@@ -8,5 +8,7 @@ const orderRouter = express.Router();
 
 orderRouter.post('/new/order', isUserAuthenticated, createNewOrder)
 orderRouter.get('/admin/get/:orderId', isUserAuthenticated, roleBasedAccess('admin'), getSingleOrder)
+orderRouter.get('/user/orders', isUserAuthenticated, getMyAllOrders)
+orderRouter.get('/admin/orders', isUserAuthenticated, roleBasedAccess('admin'), getAllOrders)
 
 export default orderRouter;
